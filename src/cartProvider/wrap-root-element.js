7b88e6054438172f90255cc6c9b3d73079fe8d78
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-01-27 16:27:13 +0500

    cart

--- src/cartProvider/wrap-root-element.js
+++ src/cartProvider/wrap-root-element.js
@@ -6,14 +6,13 @@ import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe('pk_test_51ID4h4CFr2HaNF8LAl2XJYQtyyWzDz0qxdXmJ9MVhRWvK7o0j7nuu8tV5yfYZAPYm424yYHVA1DaHxMNEfjFBKTu00d3prYB2B')
 
 export const wrapRootElement = ({element}) => {
-    console.log('location', window.location)
     return (
         <div>
         <CartProvider
             mode='client-only'
             stripe={stripePromise}
-            successUrl={`${window.location.origin}/payment-success`}
-            cancelUrl={`${window.location.origin}/`}
+            successUrl={`${window.origin}/payment-success`}
+            cancelUrl={`${window.origin}/`}
             currency="USD"
              >
 
@@ -24,6 +23,6 @@ export const wrapRootElement = ({element}) => {
     )
 }
 
-// "http://localhost:8888/payment-success"
+
 
 
